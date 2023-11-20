3c88848df25731847d61fbbd0d8cff505e2a1d1e
Gunārs Ābeltiņš <gunars2003@gmail.com>
2023-11-20 12:29:14 +0200

    Update

---
+++ Lecture/week_1_example.py
@@ -0,0 +1,12 @@
+
+print()
+
+data = input("Ievadiet veselu skaitli: ")
+data_int = int(data)
+
+if data_int > 0:
+    print("Skaitlis ir pozitīvs")
+else:
+    print("Skaitlis ir mazāks vai vienāds ar 0")
+
+print()
